732de1fe0f4a6b3da31645f1e70422495c9b460d
s1f10220288 <s1f102202886@iniad.org>
2023-07-06 11:58:14 +0900

    requirements.txt追加

--- todo/tests.py
+++ todo/tests.py
@@ -53,13 +53,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
-class django.test.Client(TestCase):
-    client=Client()
-    response=client.get("/")
 
-    self.assertEqual(response.status_code,200)
-    self.assertEqual(response.templates[0].name,"todo/index.html")
-    self.assertEqual(len(response.context["tasks"]),0)
 
 
 class TodoViewTestCase(TestCase):
@@ -69,7 +63,7 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name,"todo/index.html")
-        self.assertEqual(len(response.context["task"]),0)
+        self.assertEqual(len(response.context["tasks"]),0)
 
     
     def test_index_post(self):
@@ -79,13 +73,13 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name,"todo/index.html")
-        self.assertEqual(len(response.context["task"]),1)
+        self.assertEqual(len(response.context["tasks"]),1)
 
     def test_index_get_order_post(self):
         task1=Task(title="task1",due_at=timezone.make_aware(datetime(2023,7,1)))
         task1.save()
         task2=Task(title="task2",due_at=timezone.make_aware(datetime(2023,8,1)))
-        task1.save()
+        task2.save()
         client=Client()
         response=client.get("/?order=post")
 
@@ -94,3 +88,27 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context["tasks"][0],task2)
         self.assertEqual(response.context["tasks"][1],task1)
 
+    def test_index_get_order_due(self):
+        task1=Task(title="task1",due_at=timezone.make_aware(datetime(2023,7,1)))
+        task1.save()
+        task2=Task(title="task2",due_at=timezone.make_aware(datetime(2023,8,1)))
+        task2.save()
+        client=Client()
+        response=client.get("/?order=due")
+
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.templates[0].name,"todo/index.html")
+        self.assertEqual(response.context["tasks"][0],task1)
+        self.assertEqual(response.context["tasks"][1],task2)
+
+
+
+"""
+class django.test.Client(TestCase):
+    client=Client()
+    response=client.get("/")
+
+    self.assertEqual(response.status_code,200)
+    self.assertEqual(response.templates[0].name,"todo/index.html")
+    self.assertEqual(len(response.context["tasks"]),0)
+"""
